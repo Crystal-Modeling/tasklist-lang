0254d93b7f46213b742408d032348f1285596b47
Gregory Popov <tordektopor@gmail.com>
2023-05-22 16:06:43 +0100

    Add explanatory comments to AST->semantic mapping

--- src/language-server/task-list/source-model/task-list-semantic-reconciler.ts
+++ src/language-server/task-list/source-model/task-list-semantic-reconciler.ts
@@ -23,12 +23,29 @@ export class TaskListSemanticModelReconciler {
             .map(getSemanticTaskId)
             .filter((targetTaskId): targetTaskId is string => !!targetTaskId)
 
-        const model: ast.Model = document.parseResult.value
+        /* NOTE: So, the problem can be characterized as following:
+        
+        - I do mapping from existing structure (AST), not optimized for search element by identifier (name)
+        - I do mapping to semantic model, which I have control for, therefore, can make it indexed, and optimized for data manipulations
+        - That is why I traverse the source model!
+        - When the source model (AST) is not linear (with nested submodels), I do traversing in several iterations.
+        In previous iteration I prepare data for the next iteration (`targetTasksByMappedSourceTaskId`)
+        - I need the concept of source model _validity_, that is, which node from AST do I map to SemanticModel?
+          = which AST node I assume correct enough to track his identity?
+        - 
+        */
+
+        // Preparation: getting services, and AST root
         const semanticModelIndex = this.semanticModelState.get(document.textDocument.uri)
+        const model: ast.Model = document.parseResult.value
+
+        // NOTE: ITERATION 1: mapping Tasks
         const newTasks: ast.Task[] = []
         const existingUnmappedTasks: Map<string, SemanticTask> = semanticModelIndex.tasksByName
+        // Collecting data for the next iteration (source task id + target task => Transition). Notice, that I replaced
+        // source task with source task id (using already mapped data to optimize further mapping)
         const targetTasksByMappedSourceTaskId: MultiMap<string, ast.Task> = new MultiMap()
-
+        // Actual mapping: marking semantic elements for deletion, and AST nodes to be added
         model.tasks.forEach(task => {
             if (isCorrectlyNamed(task)) {
                 const semanticTask = existingUnmappedTasks.get(task.name)
@@ -40,11 +57,13 @@ export class TaskListSemanticModelReconciler {
                 }
             }
         })
+        // Deletion can happen immediately after the mapping within the iteration (always?)
         semanticModelIndex.removeTasksWithRelatedTransitions(existingUnmappedTasks.values())
 
+        //NOTE: ITERATION 2: mapping Transitions
         const newTransitionsForMappedSourceTaskId: [string, ast.Task][] = []
         const existingUnmappedTransitions = semanticModelIndex.transitionsBySourceTaskIdAndTargetTaskId
-
+        // Actual mapping
         targetTasksByMappedSourceTaskId.entries().forEach(([mappedSourceTaskId, targetTask]) => {
             const targetTaskId = this.semanticModelState.getTaskId(targetTask)
             if (!targetTaskId || !existingUnmappedTransitions.delete([mappedSourceTaskId, targetTaskId])) {
@@ -53,6 +72,7 @@ export class TaskListSemanticModelReconciler {
         })
         semanticModelIndex.removeTransitions(existingUnmappedTransitions.values())
 
+        //NOTE: POST-ITERATION: now unmapped source elements can be added to semantic model
         semanticModelIndex.addTasksWithTransitionsFrom(newTasks, getValidTargetSemanticTaskIds)
         semanticModelIndex.addTransitionsForSourceTaskId(newTransitionsForMappedSourceTaskId, getSemanticTaskId)
     }
