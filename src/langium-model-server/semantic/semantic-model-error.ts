62cb70a4b8314c0ee1bc5885f06a9bbb6c992d4d
Gregory Popov <tordektopor@gmail.com>
2023-05-30 19:28:36 +0100

    Rename SemanticModelError

--- src/langium-model-server/semantic/semantic-model-error.ts
+++ src/langium-model-server/semantic/semantic-model-error.ts
@@ -1,4 +1,4 @@
-export class LangiumModelServerError extends Error {
+export class SemanticModelError extends Error {
     // eslint-disable-next-line @typescript-eslint/no-parameter-properties
     constructor(message: string, override readonly cause?: unknown) {
         super(message)
