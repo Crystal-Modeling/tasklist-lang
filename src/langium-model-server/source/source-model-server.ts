238bba87b0dec18cf653eda83eed0b463382ca5b
Gregory Popov <tordektopor@gmail.com>
2023-06-02 14:07:47 +0100

    Add self-signed SSL to model server to facilitate debugging

--- src/langium-model-server/source/source-model-server.ts
+++ src/langium-model-server/source/source-model-server.ts
@@ -1,11 +1,20 @@
 import * as http2 from 'http2'
+import * as fs from 'fs-extra'
 import type { LangiumModelServerAddedServices } from '../langium-model-server-module'
 import { LangiumModelServerRouter } from './source-model-router'
+import path from 'path'
 
 export function startLangiumModelServer(lmsServices: LangiumModelServerAddedServices): http2.Http2Server {
     const router = LangiumModelServerRouter(lmsServices)
-    return http2.createServer()
+    const keyPath = path.join(__dirname, '../../ssl/key.pem')
+    const certPath = path.join(__dirname, '../../ssl/cert.pem')
+    console.debug('Trying to load SSL files from', keyPath, certPath)
+
+    return http2.createSecureServer({
+        key: fs.readFileSync(keyPath),
+        cert: fs.readFileSync(certPath)
+    })
         .on('sessionError', console.error)
         .on('stream', router)
-        .listen(8080)
+        .listen(8443)
 }
