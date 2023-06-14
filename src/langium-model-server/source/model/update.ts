3170f3379cfff6d0f2698c49003e565fc9a467bd
Gregory Popov <tordektopor@gmail.com>
2023-06-14 20:40:23 +0100

    Simplify Update types definition

--- src/langium-model-server/source/model/update.ts
+++ src/langium-model-server/source/model/update.ts
@@ -5,7 +5,7 @@ import type { ArrayUpdate } from './array-update'
  * Describes changes made to SourceModel element of type T
  */
 export type Update<T extends id.SemanticIdentity> =
-    id.SemanticIdentity & AttributesUpdate<Omit<T, keyof id.SemanticIdentity>> & NestedModelsChanges<Omit<T, keyof id.SemanticIdentity>>
+    id.SemanticIdentity & ModelChanges<Omit<T, keyof id.SemanticIdentity>>
 export namespace Update {
 
     export function isEmpty<T extends id.SemanticIdentity>(update: Update<T>): boolean {
@@ -18,14 +18,14 @@ export namespace Update {
     }
 }
 
-type AttributesUpdate<T> = Partial<ModelAttributeChanges<T> & ModelAttributesDeletion<T>>
+type ModelChanges<T> = ModelAttributeChanges<T> & ModelAttributesDeletion<T> & NestedModelsChanges<T>
 
 type ModelAttributeChanges<T> = {
-    [P in KeysOfType<T, ModelAttribute>]: T[P]
+    [P in KeysOfType<T, ModelAttribute>]?: T[P]
 }
 
 type ModelAttributesDeletion<T> = {
-    [P in OptionalKeys<T> as `__remove${Capitalize<string & P>}`]: true
+    [P in OptionalKeys<T> as `__remove${Capitalize<string & P>}`]?: true
 }
 
 type NestedModelsChanges<T> = {
